d353011d8f23693731fc869a331630e5914d8fc9
Taha Bouhoun <43461443+Tahahaha7@users.noreply.github.com>
2020-03-04 10:51:41 -0300

    Add files via upload

---
+++ Visual Studio/races.sql
@@ -0,0 +1,71 @@
+.mode column
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE Runners (
+    RUNNERID INTEGER PRIMARY KEY,
+    FIRSTNAME VARCHAR(20),
+    SURNAME VARCHAR(20),
+    RATING NUMERIC(11, 2)
+);
+
+CREATE TABLE Races (
+    RACEID INTEGER PRIMARY KEY,
+    NAME VARCHAR(20)
+);
+
+CREATE TABLE Challenges (
+    CHALLENGEID INTEGER PRIMARY KEY,
+    NAME VARCHAR(20)
+);
+
+CREATE TABLE Events (
+    CHALLENGE_KEY INTEGER,
+    RACE_KEY INTEGER,
+    FOREIGN KEY (CHALLENGE_KEY) REFERENCES Challenges(CHALLENGEID),
+    FOREIGN KEY (RACE_KEY) REFERENCES Races(RACEID),
+    PRIMARY KEY (CHALLENGE_KEY, RACE_KEY)
+);
+
+CREATE TABLE Participation (
+    RUNNER_KEY INTEGER,
+    RACE_KEY INTEGER,
+    RUNTIME INTEGER, -- Minutes
+    FOREIGN KEY (RACE_KEY) REFERENCES Races(RACEID),
+    FOREIGN KEY (RUNNER_KEY) REFERENCES Runners(RUNNERID),
+    PRIMARY KEY (RUNNER_KEY, RACE_KEY)
+);
+
+-- INNSERT DATA IN THE DATABASE
+
+INSERT INTO Challenges VALUES (1, 'The marathon challenge');
+INSERT INTO Challenges VALUES (2, 'The terrain challenge');
+
+INSERT INTO Races VALUES (1, 'The ruby marathon');
+INSERT INTO Races VALUES (2, 'The bridge challenge');
+INSERT INTO Races VALUES (3, 'The sea to mountain sprint');
+INSERT INTO Races VALUES (4, 'Flat and fast marathon');
+INSERT INTO Races VALUES (5, 'The wine route stroll');
+
+INSERT INTO Runners VALUES (1, 'Tom', 'Will', 7.1);
+INSERT INTO Runners VALUES (2, 'Steven', 'Hill', 6.4);
+INSERT INTO Runners VALUES (3, 'Sara', 'Queens', 5.9);
+INSERT INTO Runners VALUES (4, 'Sam', 'Claire', 6.6);
+
+-- Relational information
+
+INSERT INTO Events VALUES (1, 1);
+INSERT INTO Events VALUES (1, 4);
+INSERT INTO Events VALUES (2, 1);
+INSERT INTO Events VALUES (2, 2);
+INSERT INTO Events VALUES (2, 3);
+
+INSERT INTO Participation VALUES (1, 1, 10);
+INSERT INTO Participation VALUES (1, 5, 14);
+INSERT INTO Participation VALUES (2, 2, 15);
+INSERT INTO Participation VALUES (2, 3, 11);
+INSERT INTO Participation VALUES (3, 4, 20);
+INSERT INTO Participation VALUES (3, 5, 16);
+INSERT INTO Participation VALUES (3, 1, 19);
+INSERT INTO Participation VALUES (4, 2, 21);
+
+-- QUERIES
