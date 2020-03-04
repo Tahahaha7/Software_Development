dacebaed8002fc9ab4a2a6bad71ade470cc0f3a7
Taha Bouhoun <43461443+Tahahaha7@users.noreply.github.com>
2020-03-04 11:00:36 -0300

    Update races.sql

--- Visual Studio/races.sql
+++ Visual Studio/races.sql
@@ -7,6 +7,8 @@ CREATE TABLE Runners (
     RUNNERID INTEGER PRIMARY KEY,
     FIRSTNAME VARCHAR(20),
     SURNAME VARCHAR(20),
+    GENDER VARCHAR(1),
+    EMAIL VARCHAR(20),
     RATING NUMERIC(11, 2)
 );
 
@@ -48,10 +50,10 @@ INSERT INTO Races VALUES (3, 'The sea to mountain sprint');
 INSERT INTO Races VALUES (4, 'Flat and fast marathon');
 INSERT INTO Races VALUES (5, 'The wine route stroll');
 
-INSERT INTO Runners VALUES (1, 'Tom', 'Will', 7.1);
-INSERT INTO Runners VALUES (2, 'Steven', 'Hill', 6.4);
-INSERT INTO Runners VALUES (3, 'Sara', 'Queens', 5.9);
-INSERT INTO Runners VALUES (4, 'Sam', 'Claire', 6.6);
+INSERT INTO Runners VALUES (1, 'Tom', 'Will', 'M', 'tom@gmail.com', 7.1);
+INSERT INTO Runners VALUES (2, 'Steven', 'Hill', 'M', 'steven@gmail.com', 6.4);
+INSERT INTO Runners VALUES (3, 'Sara', 'Queens', 'F', 'sara@gmail.com', 5.9);
+INSERT INTO Runners VALUES (4, 'Sam', 'Claire', 'F', 'sam@gmail.com', 6.6);
 
 -- RELATIONAL INFORMATION
 
