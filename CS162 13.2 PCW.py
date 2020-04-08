e5062dd3cfefbbbb4374a960a871eeb9be2fa176
Taha Bouhoun <43461443+Tahahaha7@users.noreply.github.com>
2020-04-08 08:51:04 -0500

    Update and rename test.py to CS162 13.2 PCW.py

---
+++ CS162 13.2 PCW.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cs162_user:cs162_password@db/cs162'
+db = SQLAlchemy(app)
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    status = db.Column(db.Boolean)
+    
+    
+@app.route('/')
+def index():
+    incomplete = Entry.query.filter_by(status=True).all()
+    return render_template('index.html', element=element)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    element = Entry(text=request.form['item'], complete=False)
+    db.session.add(element)
+    db.session.commit()
+    
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
